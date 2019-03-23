46a4863f55e4faaa5015ef9c33c55ea64b756214
SamuelC28 <samuel@noukod.com>
2019-03-23 12:25:31 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,27 @@
+
+
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  //dispatch should take in an action, update the state using the reducer, and call the render function.
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  dispatch({ type: '@@INIT'})
+
+  //getState should return the current state.
+  function getState() {
+    return state;
+  }
+  
+  //return an object with two methods - getState, dispatch
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
